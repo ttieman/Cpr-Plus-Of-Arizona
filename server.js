60d68d86aad6787a4de3ca8dd82e3d807705a5b0
ttieman <tiemantanner@gmail.com>
2023-04-26 22:25:13 -0700

    footer progress

--- server.js
+++ server.js
@@ -24,13 +24,14 @@ app.get('/about', (req, res) => {
 });
 
 // send the user to index html page inspite of the url
+app.get(`/home`, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get(`/home`, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
